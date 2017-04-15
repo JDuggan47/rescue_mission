2e6428d5355c416618362f49a36ced95c5f8fce9
John Duggan <JLDuggan87@gmail.com>
2017-04-15 17:38:15 -0400

    added a board class

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+question_attributes = [
+  { title: 'Who is the best character in Saga?', description: 'Analysis of the best character in Saga'  },
+  { title: 'What happened in the Battle of Endor?', description: 'Discuss the parties involved and the reason of the conflict'}
+]
+
+question_attributes.each do |attributes|
+  question = Question.new(attributes)
+  question.save
+end
